c59ec788ff40e074034ef08cd2459e3188ad33fb
Rodrigo Mamede <rodrigomamede53@gmail.com>
2021-09-12 13:00:06 -0300

    add socket

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import fs from 'fs'
 import logger from './logger'
 import { App } from './App'
+import { Server } from 'socket.io';
 
 const PORT = process.env.PORT || 3333
 
@@ -10,7 +11,12 @@ const localHostSSL = {
 }
 
 const app = new App();
+const io = new Server({
+    cors:{ origin:'*',credentials:false}
+})
+
 const server = app.sslServer(localHostSSL);
+io.on("connection", (socket)=> logger.info(`some connected: ${socket.id}`))
 
 server.listen(PORT, () => {
     logger.info(`app running at https://localhost:${PORT} `)
